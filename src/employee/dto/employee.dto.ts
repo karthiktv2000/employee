5dc1eab55a1364019810261b8e8d6f83b5c3f65d
Karthik-S-L-YML <karthik.sl@ymedialabs.com>
2022-10-13 15:50:23 +0530

    initial commit

---
+++ src/employee/dto/employee.dto.ts
@@ -0,0 +1,40 @@
+import {
+    Equals,
+    IsAlpha,
+    IsEmail,
+    IsNumber,
+    IsPhoneNumber,
+    MaxLength,
+    MinLength,
+  } from 'class-validator';
+  
+  export class registrationDTO {
+    @IsAlpha('en-US')
+    name: string;
+  
+    @IsEmail({
+      message: 'Must be in right email format',
+    })
+    email: string;
+  
+    @MinLength(8, {
+      message: 'Password: Minimum length must be 8',
+    })
+    password: string;
+  
+    @MinLength(10, {
+      message: 'Phone: Minimum must be 10',
+    })
+    @MaxLength(10, {
+      message: 'Phone: Minimum must be 10',
+    })
+    phone: number;
+  
+    address: string;
+  
+    @IsNumber()
+    salary: number;
+  
+    role: string;
+  }
+  
